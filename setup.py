5aa5ad0f2566f58887a3b17317790b6e526cb2b7
RainingComputers <vishnu.vish.shankar@gmail.com>
2021-06-14 22:44:44 +0530

    Updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="syncgit",
-    version="0.1.3",
+    version="0.1.4",
     author="RainingComputers",
     author_email="vishnu.vish.shankar@gmail.com",
     description="Sync python dicts, strings ad modules to git repository.",
