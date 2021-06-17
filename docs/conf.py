738a5fd5d912ae19faeae55b3e5f2740f53a69d5
RainingComputers <vishnu.vish.shankar@gmail.com>
2021-06-17 22:34:05 +0530

    Update version

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ copyright = '2021, Vishnu Shankar B'
 author = 'Vishnu Shankar B'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.4'
+release = '0.2.4'
 
 
 # -- General configuration ---------------------------------------------------
